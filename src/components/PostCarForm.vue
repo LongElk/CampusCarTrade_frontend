3b56264b9ff84ded2cdbab829d0d009aaabaff11
whpz <2206373437@qq.om>
2025-07-01 18:43:03 +0800

    7.1

--- src/components/PostCarForm.vue
+++ src/components/PostCarForm.vue
@@ -5,7 +5,10 @@
     </el-form-item>
 
     <el-form-item label="类型">
-      <el-input v-model="form.type" />
+      <el-select v-model="form.type" placeholder="请选择类型">
+        <el-option label="自行车" value="BICYCLE" />
+        <el-option label="电动车" value="ELECTRIC" />
+      </el-select>
     </el-form-item>
 
     <el-form-item label="描述">
@@ -51,7 +54,7 @@ import { Plus } from '@element-plus/icons-vue'
 
 const form = ref<PublishCarParams>({
   title: '',
-  type: '',
+  type: 'BICYCLE',
   description: '',
   price: 0,
   mileage: 0,
@@ -79,7 +82,9 @@ const submit = async () => {
     const imageUrls: string[] = []
     for (let i = 0; i < files.value.length; i++) {
       const res = await uploadCarImage(files.value[i], i)
-      if (res.code === 0 && res.data.url) {
+      console.log(res.code)
+      if (res.code === 200 && res.data.url) {
+        console.log(`Uploaded image ${i + 1}/${files.value.length}:`, res.data.url)
         imageUrls.push(res.data.url)
       }
     }
